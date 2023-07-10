2c865e5dd4cbf0710093d3fc6559a2e09c4b58f0
techigh <techigh.b@gmail.com>
2023-07-10 21:49:49 +0900

    scroll animation

--- src/features/about/views/AboutView.vue
+++ src/features/about/views/AboutView.vue
@@ -1,39 +1,125 @@
 <template>
-  <div class="min-h-screen border border-red-500">
-    <div class="w-full border border-indigo-500">
+  <div class="min-h-screen">
+    <div class="sticky top-96 w-full">
       <p
         id="brand-name"
-        class="w-fit border m-auto uppercase font-bold text-7xl"
+        class="absolute w-full h-full flex items-center justify-center uppercase font-bold text-black opacity-10 text-7xl origin-bottom"
       >
         marvel fitness
       </p>
     </div>
-    <div class="h-[300vh]"></div>
-    <!-- <div class="h-96 mt-20 px-10 py-10 flex gap-x-4 bg-gray-100">
-      <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
-        <img src="@assets/example1.png" alt="" class="h-full object-contain" />
+    <div id="contents-container" class="h-[250vh] bg-black/0"></div>
+    <div class="bg-red-500">
+      <div class="h-96 mt-20 px-10 py-10 flex gap-x-4 bg-gray-100">
+        <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
+          <img
+            src="@assets/example1.png"
+            alt=""
+            class="h-full object-contain"
+          />
+        </div>
+        <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
+          <img
+            src="@assets/example2.png"
+            alt=""
+            class="h-full object-contain"
+          />
+        </div>
+        <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
+          <img
+            src="@assets/example4.png"
+            alt=""
+            class="h-full object-contain"
+          />
+        </div>
+        <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
+          <img src="@assets/image2.jpg" alt="" class="h-full object-contain" />
+        </div>
       </div>
-      <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
-        <img src="@assets/example2.png" alt="" class="h-full object-contain" />
-      </div>
-      <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
-        <img src="@assets/example4.png" alt="" class="h-full object-contain" />
-      </div>
-      <div class="min-w-[5rem] rounded-lg bg-black overflow-hidden">
-        <img src="@assets/image2.jpg" alt="" class="h-full object-contain" />
-      </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from "vue";
+let brandName: HTMLElement | null;
+let contentsContainer: HTMLElement | null;
 
 onMounted(() => {
-  document.body.addEventListener("wheel", (e: WheelEvent) => {
-    console.log(window.scrollY);
+  brandName = document.getElementById("brand-name");
+  contentsContainer = document.getElementById("contents-container");
+  document.body.addEventListener("wheel", (_: WheelEvent) => {
+    requestAnimationFrame(updateScale);
+    requestAnimationFrame(updateOpacity);
+    requestAnimationFrame(updateBackgroundColor);
   });
 });
+// Text Scale
+let currentScale = 1;
+const minScale = 1;
+const maxScale = 50;
+let scaleAnimation: number;
+function updateScale() {
+  const scrollY = window.scrollY - 500;
+  const newScale = Math.max(minScale, Math.min(maxScale, scrollY * 0.03));
+
+  currentScale = newScale;
+  if (currentScale === newScale) {
+    cancelAnimationFrame(scaleAnimation);
+  } else {
+    scaleAnimation = requestAnimationFrame(updateScale);
+  }
+  if (brandName !== null) {
+    brandName.style.transform = `matrix(${newScale},0,0,${newScale},0,0)`;
+    console.log(newScale);
+    if (newScale === maxScale) {
+      brandName.style.opacity = `0`;
+    }
+  }
+}
+// Text Opacity
+let currentOpacity = 0.1;
+const minOpcaity = 0.1;
+const maxOpacity = 1;
+let opacityAnimation: number;
+function updateOpacity() {
+  const scrollY = window.scrollY;
+  const newOpacity = Math.max(
+    minOpcaity,
+    Math.min(maxOpacity, scrollY * 0.002)
+  );
+
+  currentOpacity = newOpacity;
+  if (currentOpacity === newOpacity) {
+    cancelAnimationFrame(opacityAnimation);
+  } else {
+    opacityAnimation = requestAnimationFrame(updateOpacity);
+  }
+  if (brandName !== null) {
+    brandName.style.opacity = `${newOpacity}`;
+  }
+}
+
+// Text Opacity
+let currentBgOpacity = 0;
+const minBgOpcaity = 0;
+const maxBgOpacity = 1;
+let bgOpacityAnimation: number;
+function updateBackgroundColor() {
+  const scrollY = window.scrollY - 750;
+  const newBgOpacity = Math.max(
+    minBgOpcaity,
+    Math.min(maxBgOpacity, scrollY * 0.001)
+  );
+  if (currentBgOpacity === newBgOpacity) {
+    cancelAnimationFrame(bgOpacityAnimation);
+  } else {
+    bgOpacityAnimation = requestAnimationFrame(updateScale);
+  }
+  if (contentsContainer !== null) {
+    contentsContainer.style.backgroundColor = `rgb(0 0 0 / ${newBgOpacity})`;
+  }
+}
 </script>
 
 <style scoped></style>
